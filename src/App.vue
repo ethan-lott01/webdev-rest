ae8af75a735a408b67768915e872865f03ff279d
Pedro Cruz <cruz8114@stthomas.edu>
2023-12-22 01:58:41 -0600

    fixed neighborhood marker count loop

--- src/App.vue
+++ src/App.vue
@@ -181,6 +181,17 @@ let filterInfo = reactive({
 let markers = [];
 const tableUpdateOnZoom = ref(0);
 
+
+//alternate marker color
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 // Vue callback for once <template> HTML has been added to web page
 onMounted(() => {
   // Create Leaflet map (set bounds and valied zoom levels)
@@ -369,10 +380,10 @@ function updateCrimes() {
         console.log(data);
         crimes.value = data;
         console.log(crimes.valueOf());
-        for (let j = 0; j <= map.neighborhood_markers.length; j++) {
+        for (let j = 0; j <= map.neighborhood_markers.length-1; j++) {
           map.neighborhood_markers[j].count = 0;
         }
-        for (let i = 0; i <= data.length; i++) {
+        for (let i = 0; i <= data.length-1; i++) {
           map.neighborhood_markers[data[i].neighborhood_number - 1].count++;
         }
         drawMarkers();
